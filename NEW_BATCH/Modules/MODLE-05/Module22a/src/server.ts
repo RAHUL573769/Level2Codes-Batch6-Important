28dccace943360dd525f592fca3da42df1a61c56
RAHUL RUDRA <rahulrudra665@gmail.com>
2026-01-12 10:26:21 +0600

    nextjs started

--- NEW_BATCH/Modules/MODLE-05/Module22a/src/server.ts
+++ NEW_BATCH/Modules/MODLE-05/Module22a/src/server.ts
@@ -1,42 +1,27 @@
-// import { prisma } from '../lib/prisma'
-
-import { prisma } from "../lib/prisma"
+import { prisma } from "../lib/prisma";
 
 async function main() {
-    // Create a new user with a post
-    const user = await prisma.user.create({
-        data: {
-            name: 'Alice',
-            email: 'alice@prisma.io',
-            posts: {
-                create: {
-                    title: 'Hello World',
-                    content: 'This is my first post!',
-                    published: true,
-                },
-            },
+    const user = await prisma.user.upsert({
+        where: {
+            email: "rahul@gmail.com",
         },
-        include: {
-            posts: true,
+        update: {
+            name: "aulk",
         },
-    })
-    console.log('Created user:', user)
+        create: {
+            name: "aulk",
+            email: "rahul@gmail.com",
+        },
+    });
 
-    // Fetch all users with their posts
-    const allUsers = await prisma.user.findMany({
-        include: {
-            posts: true,
-        },
-    })
-    console.log('All users:', JSON.stringify(allUsers, null, 2))
+    console.log("Final-User", user);
+
+    const getUser = await prisma.user.findMany();
+    console.log("All-Users", getUser);
 }
 
 main()
-    .then(async () => {
-        await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-    })
+    .catch(console.error)
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
