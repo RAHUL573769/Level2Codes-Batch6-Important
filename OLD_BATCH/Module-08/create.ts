c4a2e69be9c66ecd119aedaabf15df839c05bb45
RAHUL RUDRA <rahulrudra665@gmail.com>
2026-01-20 08:36:34 +0600

    changes made"

--- OLD_BATCH/Module-08/create.ts
+++ OLD_BATCH/Module-08/create.ts
@@ -3,50 +3,86 @@ import { prisma } from "./lib/prisma";
 
 const main = async () => {
     try {
-        // const createUser = await prisma.user.create({
+        const createUser = await prisma.user.create({
+            data: {
+                username: "udydsd6esssser2dsf1d601",
+                email: "userdyd26de1sssfdds016@ph6.com",
+                role: UserRole.user
+            }
+        });
+
+        // const user = await prisma.user.create({
         //     data: {
-        //         username: "user201",
-        //         email: "user210@ph.com",
-        //         role: UserRole.user
-        //     }
-        // });
+        //         username: "John Doe",
+        //     },
+        // })
+
+        const post = await prisma.post.create({
+            data: {
+                title: "My First Post",
+                content: "This is my post content",
+                authorId: createUser.id,
+            },
+        })
+
         // console.log(createUser)
 
         const createProfile = await prisma.profile.create({
             data: {
-                bio: "this is bio...",
-                userId: 1
+                bio: "this is b16io...",
+                userId: createUser.id
             }
         })
+        // console.log("Create Profile", createProfile)
 
         const createCategory = await prisma.category.create({
             data: {
-                name: "software engineering"
+                name: "software engineering1"
             }
         })
-        // const createdPost = await prisma.post.create({
-        //     data: {
-        //         title: "this is title 5",
-        //         content: "this is content of the post. 5",
-        //         authorId: 3, // user with id=3 must exist
-        //         postCategory: {
-        //             create: [
-        //                 { categoryId: 1 },
-        //                 { categoryId: 3 },
-        //                 { categoryId: 4 }
-        //             ]
-        //         }
-        //     },
-        //     include: {
-        //         postCategory: true
-        //     }
-        // });
+        // console.log("Create Category", createCategory)
+        const createdPost = await prisma.post.create({
+            data: {
+                title: "this is title 5",
+                content: "this is content of the post. 5",
+                authorId: createUser.id, // user with id=3 must exist
+                postCategory: {
+                    create: [
+                        { categoryId: 1 },
+                        { categoryId: 3 },
+                        { categoryId: 4 }
+                    ]
+                }
+            },
+            include: {
+                postCategory: true
+            }
+        });
 
         // console.log(createdPost);
+
+
+        //relation filters
+        const publishedPosts = await prisma.user.findMany({
+            include: {
+                post: { where: { published: false } }
+            }
+        })
+
+        console.dir(publishedPosts, {
+            depth: Infinity
+
+        })
+
+
+        const andFiltering = await prisma.post.findMany({
+            where: {
+
+            }
+        })
+
     } catch (error) {
         console.error("Error creating post:", error);
-    } finally {
-        await prisma.$disconnect();
     }
 };
 
