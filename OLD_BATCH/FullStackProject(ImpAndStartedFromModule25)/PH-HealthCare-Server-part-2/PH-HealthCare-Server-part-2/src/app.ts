654fa7ad4175002dc97a6ba2c7e0f0ac2cc5117b
RAHUL RUDRA <rahulrudra665@gmail.com>
2026-02-15 10:14:14 +0600

    chages made"

--- OLD_BATCH/FullStackProject(ImpAndStartedFromModule25)/PH-HealthCare-Server-part-2/PH-HealthCare-Server-part-2/src/app.ts
+++ OLD_BATCH/FullStackProject(ImpAndStartedFromModule25)/PH-HealthCare-Server-part-2/PH-HealthCare-Server-part-2/src/app.ts
@@ -10,6 +10,8 @@ app.use(cors());
 //parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use("/uploads1", express.static("uploads1"));
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send({
--- OLD_BATCH/FullStackProject(ImpAndStartedFromModule25)/PH-HealthCare-Server-part-2/PH-HealthCare-Server-part-2/src/app/modules/User/user.routes.ts
+++ OLD_BATCH/FullStackProject(ImpAndStartedFromModule25)/PH-HealthCare-Server-part-2/PH-HealthCare-Server-part-2/src/app/modules/User/user.routes.ts
@@ -5,6 +5,8 @@ import { jwtHelpers } from "../../../helpars/jwthelpers";
 import config from "../../../config";
 import { UserRole } from "../../../../generated/prisma/enums";
 import { auth } from "../../../middlewares/auth.middleware";
+import multer from "multer";
+import path from "path";
 
 const router = express.Router();
 
@@ -53,6 +55,39 @@ const validateRequest = (schema: ZodSchema) =>
 //     }
 // }
 
-router.post("/", auth(UserRole.ADMIN, UserRole.SUPER_ADMIN), userController.createAdmin);
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(process.cwd(), "uploads1"));
+    },
+    filename: function (req, file, cb) {
+
+
+        cb(null, file.originalname);
+    },
+});
+
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, '/uploads')
+//         cb(null, path(process.cwd(), "uploads"))
+//     },
+//     filename: function (req, file, cb) {
+//         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+//         cb(null, file.fieldname + '-' + uniqueSuffix)
+//         cb(null, file.originalname)
+
+//     },
+
+// })
+const upload = multer({ storage });
+console.log(upload)
+console.log(path.join(process.cwd(), "uploads1"))
+console.log(path.join(process.cwd(), "uploads1"))
+router.post("/",
+    upload.single("file"), //use file name for uploading
+    // auth(UserRole.ADMIN, UserRole.SUPER_ADMIN),
+    userController.createAdmin);
 
 export const userRoutes = router;
